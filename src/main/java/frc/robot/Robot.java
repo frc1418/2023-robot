c6f1930555f7828f80c3a61a04de82ae37238dce
BenGitsMagic <benberol40@gmail.com>
2023-01-12 16:27:21 -0500

    Swerve Drive Stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -72,6 +72,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
+    m_robotContainer.backLeftWheel.getAngleMotor().setIdleMode(IdleMode.kCoast);
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
