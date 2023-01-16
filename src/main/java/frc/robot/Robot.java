79f9b19ae8ca301c04c239fe3de4f1f897524f50
BrandonWerbel <brandon@werbel.org>
2023-01-16 15:34:40 -0500

    Remove testing code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -72,7 +72,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    // m_robotContainer.backLeftWheel.getAngleMotor().setIdleMode(IdleMode.kCoast);
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
