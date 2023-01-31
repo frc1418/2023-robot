23d5abecd0205c238738c5619c09e091de7799c0
BrandonWerbel <brandon@werbel.org>
2023-01-31 17:14:42 -0500

    Remove unused imports

--- src/main/java/frc/robot/commands/autonomous/ChargeCommand.java
+++ src/main/java/frc/robot/commands/autonomous/ChargeCommand.java
@@ -4,36 +4,16 @@
 
 package frc.robot.commands.autonomous;
 
-import frc.robot.Constants;
-import frc.robot.Constants.DrivetrainSubsystem;
 import frc.robot.commands.FollowTrajectoryCommand;
 import frc.robot.common.Odometry;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.SwerveDriveSubsystem;
 
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
-import com.pathplanner.lib.commands.PPSwerveControllerCommand;
-
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 
 /** An example command that uses an example subsystem. */
 public class ChargeCommand extends SequentialCommandGroup {
