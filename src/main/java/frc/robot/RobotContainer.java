19b8da8f75a5c4433a89d6becd34184d144a23bf
BrandonWerbel <brandon@werbel.org>
2023-02-10 17:40:04 -0500

    working basic superstructure

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,8 @@ package frc.robot;
 import java.lang.System.Logger.Level;
 import java.util.HashMap;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
@@ -62,63 +64,63 @@ public class RobotContainer {
 
     private final RobotBase robot;
 
-    private CANSparkMax backRightAngleMotor = new CANSparkMax(DrivetrainConstants.BACK_RIGHT_ANGLE_ID, MotorType.kBrushless);
-    private CANSparkMax backRightSpeedMotor = new CANSparkMax(DrivetrainConstants.BACK_RIGHT_SPEED_ID, MotorType.kBrushless);
-    private AnalogEncoder backRightEncoder = new AnalogEncoder(DrivetrainConstants.BACK_RIGHT_ENCODER);
+    // private CANSparkMax backRightAngleMotor = new CANSparkMax(DrivetrainConstants.BACK_RIGHT_ANGLE_ID, MotorType.kBrushless);
+    // private CANSparkMax backRightSpeedMotor = new CANSparkMax(DrivetrainConstants.BACK_RIGHT_SPEED_ID, MotorType.kBrushless);
+    // private AnalogEncoder backRightEncoder = new AnalogEncoder(DrivetrainConstants.BACK_RIGHT_ENCODER);
 
-    private CANSparkMax frontRightAngleMotor = new CANSparkMax(DrivetrainConstants.FRONT_RIGHT_ANGLE_ID, MotorType.kBrushless);
-    private CANSparkMax frontRightSpeedMotor = new CANSparkMax(DrivetrainConstants.FRONT_RIGHT_SPEED_ID, MotorType.kBrushless);
-    private AnalogEncoder frontRightEncoder = new AnalogEncoder(DrivetrainConstants.FRONT_RIGHT_ENCODER);
+    // private CANSparkMax frontRightAngleMotor = new CANSparkMax(DrivetrainConstants.FRONT_RIGHT_ANGLE_ID, MotorType.kBrushless);
+    // private CANSparkMax frontRightSpeedMotor = new CANSparkMax(DrivetrainConstants.FRONT_RIGHT_SPEED_ID, MotorType.kBrushless);
+    // private AnalogEncoder frontRightEncoder = new AnalogEncoder(DrivetrainConstants.FRONT_RIGHT_ENCODER);
 
-    private CANSparkMax backLeftAngleMotor = new CANSparkMax(DrivetrainConstants.BACK_LEFT_ANGLE_ID, MotorType.kBrushless);
-    private CANSparkMax backLeftSpeedMotor = new CANSparkMax(DrivetrainConstants.BACK_LEFT_SPEED_ID, MotorType.kBrushless);
-    private AnalogEncoder backLeftEncoder = new AnalogEncoder(DrivetrainConstants.BACK_LEFT_ENCODER);
+    // private CANSparkMax backLeftAngleMotor = new CANSparkMax(DrivetrainConstants.BACK_LEFT_ANGLE_ID, MotorType.kBrushless);
+    // private CANSparkMax backLeftSpeedMotor = new CANSparkMax(DrivetrainConstants.BACK_LEFT_SPEED_ID, MotorType.kBrushless);
+    // private AnalogEncoder backLeftEncoder = new AnalogEncoder(DrivetrainConstants.BACK_LEFT_ENCODER);
 
-    private CANSparkMax frontLeftAngleMotor = new CANSparkMax(DrivetrainConstants.FRONT_LEFT_ANGLE_ID, MotorType.kBrushless);
-    private CANSparkMax frontLeftSpeedMotor = new CANSparkMax(DrivetrainConstants.FRONT_LEFT_SPEED_ID, MotorType.kBrushless);
-    private AnalogEncoder frontLeftEncoder = new AnalogEncoder(DrivetrainConstants.FRONT_LEFT_ENCODER);
+    // private CANSparkMax frontLeftAngleMotor = new CANSparkMax(DrivetrainConstants.FRONT_LEFT_ANGLE_ID, MotorType.kBrushless);
+    // private CANSparkMax frontLeftSpeedMotor = new CANSparkMax(DrivetrainConstants.FRONT_LEFT_SPEED_ID, MotorType.kBrushless);
+    // private AnalogEncoder frontLeftEncoder = new AnalogEncoder(DrivetrainConstants.FRONT_LEFT_ENCODER);
 
-    private WheelSubsystem backRightWheel = new WheelSubsystem (
-        backRightAngleMotor, backRightSpeedMotor, backRightEncoder,
-        DrivetrainConstants.BACK_RIGHT_LOC);
-    public WheelSubsystem backLeftWheel = new WheelSubsystem (
-      backLeftAngleMotor, backLeftSpeedMotor, backLeftEncoder,
-      DrivetrainConstants.BACK_LEFT_LOC);
-    private WheelSubsystem frontRightWheel = new WheelSubsystem (
-      frontRightAngleMotor, frontRightSpeedMotor, frontRightEncoder,
-      DrivetrainConstants.FRONT_RIGHT_LOC);
-    private WheelSubsystem frontLeftWheel = new WheelSubsystem (
-      frontLeftAngleMotor, frontLeftSpeedMotor, frontLeftEncoder,
-      DrivetrainConstants.FRONT_LEFT_LOC);
+    // private WheelSubsystem backRightWheel = new WheelSubsystem (
+    //     backRightAngleMotor, backRightSpeedMotor, backRightEncoder,
+    //     DrivetrainConstants.BACK_RIGHT_LOC);
+    // public WheelSubsystem backLeftWheel = new WheelSubsystem (
+    //   backLeftAngleMotor, backLeftSpeedMotor, backLeftEncoder,
+    //   DrivetrainConstants.BACK_LEFT_LOC);
+    // private WheelSubsystem frontRightWheel = new WheelSubsystem (
+    //   frontRightAngleMotor, frontRightSpeedMotor, frontRightEncoder,
+    //   DrivetrainConstants.FRONT_RIGHT_LOC);
+    // private WheelSubsystem frontLeftWheel = new WheelSubsystem (
+    //   frontLeftAngleMotor, frontLeftSpeedMotor, frontLeftEncoder,
+    //   DrivetrainConstants.FRONT_LEFT_LOC);
     
 
     AHRS gyro = new AHRS(SPI.Port.kMXP);
 
-    private SwerveModulePosition[] positions = new SwerveModulePosition[] {
-      frontLeftWheel.getSwerveModulePosition(),
-      frontRightWheel.getSwerveModulePosition(),
-      backLeftWheel.getSwerveModulePosition(),
-      backRightWheel.getSwerveModulePosition()
-    };
+    // private SwerveModulePosition[] positions = new SwerveModulePosition[] {
+    //   frontLeftWheel.getSwerveModulePosition(),
+    //   frontRightWheel.getSwerveModulePosition(),
+    //   backLeftWheel.getSwerveModulePosition(),
+    //   backRightWheel.getSwerveModulePosition()
+    // };
 
-    private SwerveDriveOdometry driveOdometry = new SwerveDriveOdometry(DrivetrainConstants.SWERVE_KINEMATICS, gyro.getRotation2d(), positions);
+    // private SwerveDriveOdometry driveOdometry = new SwerveDriveOdometry(DrivetrainConstants.SWERVE_KINEMATICS, gyro.getRotation2d(), positions);
 
-    private Odometry odometry = new Odometry(gyro, driveOdometry, positions);
+    // private Odometry odometry = new Odometry(gyro, driveOdometry, positions);
     
-    private SwerveDriveSubsystem swerveDrive = new SwerveDriveSubsystem(
-        backRightWheel, backLeftWheel, frontRightWheel, frontLeftWheel,
-        DrivetrainConstants.SWERVE_KINEMATICS, odometry);
+    // private SwerveDriveSubsystem swerveDrive = new SwerveDriveSubsystem(
+    //     backRightWheel, backLeftWheel, frontRightWheel, frontLeftWheel,
+    //     DrivetrainConstants.SWERVE_KINEMATICS, odometry);
 
 
-    private CANSparkMax pivotMotor = new CANSparkMax(100, MotorType.kBrushless);
-    private Talon telescopeMotor = new Talon(100);
+    private CANSparkMax pivotMotor = new CANSparkMax(10, MotorType.kBrushless);
+    private TalonFX telescopeMotor = new TalonFX(11);
     private ArmSubsystem armSubsystem = new ArmSubsystem(pivotMotor, telescopeMotor);
 
-    private DoubleSolenoid leftSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 0);
-    private DoubleSolenoid rightSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 0);
-    private GrabberSubsystem grabberSubsystem = new GrabberSubsystem(leftSolenoid, rightSolenoid);
+    // private DoubleSolenoid leftSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 0);
+    // private DoubleSolenoid rightSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, 0, 0);
+    // private GrabberSubsystem grabberSubsystem = new GrabberSubsystem(leftSolenoid, rightSolenoid);
 
-    private CANSparkMax elevatorMotor = new CANSparkMax(100, MotorType.kBrushless);
+    private CANSparkMax elevatorMotor = new CANSparkMax(9, MotorType.kBrushless);
     private ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem(elevatorMotor);
 
     // LOAD TRAJECTORIES
@@ -127,7 +129,7 @@ public class RobotContainer {
 
     // SENDABLE CHOOSER
     private final SendableChooser<Command> chooser = new SendableChooser<>();
-    private final Command chargeCommand = new ChargeCommand(swerveDrive, odometry, trajectories);
+    // private final Command chargeCommand = new ChargeCommand(swerveDrive, odometry, trajectories);
 
     private final LevelChargingStationCommand levelChargingStationCommand = new LevelChargingStationCommand(odometry, swerveDrive);
 
@@ -135,8 +137,8 @@ public class RobotContainer {
     public RobotContainer(RobotBase robot) {
       this.robot = robot;
 
-      chooser.setDefaultOption("Charge Command", chargeCommand);
-      chooser.addOption("Command", chargeCommand);
+      // chooser.setDefaultOption("Charge Command", chargeCommand);
+      // chooser.addOption("Command", chargeCommand);
       SmartDashboard.putData(chooser);
 
       // Configure the button bindings
@@ -148,15 +150,15 @@ public class RobotContainer {
       // frontLeftAngleMotor.setInverted(true);
       // backLeftAngleMotor.setInverted(true);
 
-      frontLeftAngleMotor.setIdleMode(IdleMode.kBrake);
-      frontRightAngleMotor.setIdleMode(IdleMode.kBrake);
-      backLeftAngleMotor.setIdleMode(IdleMode.kBrake);
-      backRightAngleMotor.setIdleMode(IdleMode.kBrake);
+      // frontLeftAngleMotor.setIdleMode(IdleMode.kBrake);
+      // frontRightAngleMotor.setIdleMode(IdleMode.kBrake);
+      // backLeftAngleMotor.setIdleMode(IdleMode.kBrake);
+      // backRightAngleMotor.setIdleMode(IdleMode.kBrake);
 
-      frontLeftSpeedMotor.setIdleMode(IdleMode.kBrake);
-      frontRightSpeedMotor.setIdleMode(IdleMode.kBrake);
-      backLeftSpeedMotor.setIdleMode(IdleMode.kBrake);
-      backRightSpeedMotor.setIdleMode(IdleMode.kBrake);
+      // frontLeftSpeedMotor.setIdleMode(IdleMode.kBrake);
+      // frontRightSpeedMotor.setIdleMode(IdleMode.kBrake);
+      // backLeftSpeedMotor.setIdleMode(IdleMode.kBrake);
+      // backRightSpeedMotor.setIdleMode(IdleMode.kBrake);
 
       // frontLeftAngleMotor.setInverted(true);
       // frontRightAngleMotor.setInverted(true);
@@ -182,6 +184,17 @@ public class RobotContainer {
       backLeftWheel.getEncoder().setZeroOffset(DrivetrainConstants.BACK_LEFT_ENCODER_OFFSET);
       frontRightWheel.getEncoder().setZeroOffset(DrivetrainConstants.FRONT_RIGHT_ENCODER_OFFSET);
       frontLeftWheel.getEncoder().setZeroOffset(DrivetrainConstants.FRONT_LEFT_ENCODER_OFFSET);
+      // frontRightSpeedMotor.setInverted(true);
+      // backRightSpeedMotor.setInverted(true);
+
+      // backRightEncoder.setPositionOffset(DrivetrainConstants.BACK_RIGHT_ENCODER_OFFSET);
+      // backLeftEncoder.setPositionOffset(DrivetrainConstants.BACK_LEFT_ENCODER_OFFSET);
+      // frontRightEncoder.setPositionOffset(DrivetrainConstants.FRONT_RIGHT_ENCODER_OFFSET);
+      // frontLeftEncoder.setPositionOffset(DrivetrainConstants.FRONT_LEFT_ENCODER_OFFSET);
+
+      pivotMotor.setIdleMode(IdleMode.kBrake);
+      elevatorMotor.setIdleMode(IdleMode.kBrake);
+      telescopeMotor.setNeutralMode(NeutralMode.Brake);
     }
 
     /**
@@ -191,39 +204,49 @@ public class RobotContainer {
      * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
      */
     private void configureButtonBindings() {
-      Joystick leftJoystick = new Joystick(0);
-      Joystick rightJoystick = new Joystick(1);
+      // Joystick leftJoystick = new Joystick(0);
+      // Joystick rightJoystick = new Joystick(1);
       Joystick altJoystick = new Joystick(2);
 
       
       JoystickButton balanceChargingStationButton = new JoystickButton(rightJoystick, 1);
       JoystickButton turtleButton = new JoystickButton(rightJoystick, 3);
       JoystickButton fieldCentricButton = new JoystickButton(leftJoystick, 2);
+      // JoystickButton fieldCentricButton = new JoystickButton(leftJoystick, 3);
 
-      JoystickButton pivotUpButton = new JoystickButton(altJoystick, 100);
-      JoystickButton pivotDownButton = new JoystickButton(altJoystick, 100);
+      JoystickButton pivotUpButton = new JoystickButton(altJoystick, 4);
+      JoystickButton pivotDownButton = new JoystickButton(altJoystick, 1);
 
-      JoystickButton telescopeOutButton = new JoystickButton(altJoystick, 100);
-      JoystickButton telescopeInButton = new JoystickButton(altJoystick, 100);
+      JoystickButton telescopeOutButton = new JoystickButton(altJoystick, 3);
+      JoystickButton telescopeInButton = new JoystickButton(altJoystick, 2);
 
-      JoystickButton elevatorUpButton = new JoystickButton(altJoystick, 100);
-      JoystickButton elevatorDownButton = new JoystickButton(altJoystick, 100);
+      JoystickButton elevatorUpButton = new JoystickButton(altJoystick, 6);
+      JoystickButton elevatorDownButton = new JoystickButton(altJoystick, 5);
 
-      JoystickButton toggleGrabberButton = new JoystickButton(altJoystick, 100);
+      // JoystickButton toggleGrabberButton = new JoystickButton(altJoystick, 100);
 
 
-      swerveDrive.setDefaultCommand(new RunCommand(
-          () -> {
-            if (robot.isTeleopEnabled()) {
-              swerveDrive.drive(
-                  applyDeadband(leftJoystick.getY(), DrivetrainConstants.DRIFT_DEADBAND) * DriverConstants.speedMultiplier,
-                  applyDeadband(leftJoystick.getX(),DrivetrainConstants.DRIFT_DEADBAND) * DriverConstants.speedMultiplier,
-                  applyDeadband(rightJoystick.getX() * DriverConstants.angleMultiplier, DrivetrainConstants.ROTATION_DEADBAND));
-            } else {
-              swerveDrive.drive(0, 0, 0);
-            }
-          },
-          swerveDrive));
+      // swerveDrive.setDefaultCommand(new RunCommand(
+      //     () -> {
+      //       if (robot.isTeleopEnabled()) {
+      //         swerveDrive.drive(
+      //             applyDeadband(-leftJoystick.getY(), DrivetrainConstants.DRIFT_DEADBAND) * DriverConstants.speedMultiplier,
+      //             applyDeadband(-leftJoystick.getX(),DrivetrainConstants.DRIFT_DEADBAND) * DriverConstants.speedMultiplier,
+      //             applyDeadband(-rightJoystick.getX() * DriverConstants.angleMultiplier, DrivetrainConstants.ROTATION_DEADBAND));
+      //       } else {
+      //         swerveDrive.drive(0, 0, 0);
+      //       }
+      //     },
+      //     swerveDrive));
+
+      // fieldCentricButton.onTrue(new InstantCommand(
+      //     () -> {
+      //       System.out.println("FIELD CENTRIC TOGGLED");
+      //       swerveDrive.toggleFieldCentric();
+      //     }, swerveDrive));
+
+      pivotUpButton.whileTrue(new RunCommand(() -> armSubsystem.setPivotMotor(1.5), armSubsystem));
+      pivotUpButton.onFalse(new InstantCommand(() -> armSubsystem.setPivotMotor(0.4), armSubsystem));
 
       fieldCentricButton.onTrue(new InstantCommand(
           () -> {
@@ -234,17 +257,22 @@ public class RobotContainer {
       balanceChargingStationButton.whileTrue(levelChargingStationCommand);
 
       turtleButton.whileTrue(new RunCommand(() -> swerveDrive.turtle(), swerveDrive));
+      pivotDownButton.whileTrue(new RunCommand(() -> armSubsystem.setPivotMotor(-0.1), armSubsystem));
+      pivotDownButton.onFalse(new InstantCommand(() -> armSubsystem.setPivotMotor(0), armSubsystem));
 
-      pivotUpButton.whileTrue(new InstantCommand(() -> armSubsystem.setPivotMotor(0.2), armSubsystem));
-      pivotDownButton.whileTrue(new InstantCommand(() -> armSubsystem.setPivotMotor(-0.2), armSubsystem));
+      elevatorUpButton.whileTrue(new RunCommand(() -> elevatorSubsystem.setElevatorMotor(-0.2), elevatorSubsystem));
+      elevatorUpButton.onFalse(new InstantCommand(() -> elevatorSubsystem.setElevatorMotor(0), elevatorSubsystem));
 
-      elevatorUpButton.whileTrue(new InstantCommand(() -> elevatorSubsystem.setElevatorMotor(-0.2), elevatorSubsystem));
-      elevatorDownButton.whileTrue(new InstantCommand(() -> elevatorSubsystem.setElevatorMotor(0.2), elevatorSubsystem));
+      elevatorDownButton.whileTrue(new RunCommand(() -> elevatorSubsystem.setElevatorMotor(0.2), elevatorSubsystem));
+      elevatorDownButton.onFalse(new InstantCommand(() -> elevatorSubsystem.setElevatorMotor(0), elevatorSubsystem));
 
-      telescopeOutButton.whileTrue(new InstantCommand(() -> armSubsystem.setTelescopeMotor(0.2), armSubsystem));
-      telescopeInButton.whileTrue(new InstantCommand(() -> armSubsystem.setTelescopeMotor(-0.2), armSubsystem));
+      telescopeOutButton.whileTrue(new RunCommand(() -> armSubsystem.setTelescopeMotor(35), armSubsystem));
+      telescopeOutButton.onFalse(new InstantCommand(() -> armSubsystem.setTelescopeMotor(0), armSubsystem));
 
-      toggleGrabberButton.onTrue(new InstantCommand(() -> grabberSubsystem.toggle(), grabberSubsystem));
+      telescopeInButton.whileTrue(new RunCommand(() -> armSubsystem.setTelescopeMotor(-35), armSubsystem));
+      telescopeInButton.onFalse(new InstantCommand(() -> armSubsystem.setTelescopeMotor(0), armSubsystem));
+
+      // toggleGrabberButton.onTrue(new InstantCommand(() -> grabberSubsystem.toggle(), grabberSubsystem));
     }
 
     /**
@@ -254,8 +282,8 @@ public class RobotContainer {
      */
     public Command getAutonomousCommand() {
       // An ExampleCommand will run in autonomous
-      odometry.zeroHeading();
-      return chargeCommand;
+      // odometry.zeroHeading();
+      return null;
     }
 
     public double applyDeadband(double val, double deadband){
@@ -263,19 +291,19 @@ public class RobotContainer {
       else return val;
     }
 
-    public Odometry getOdometry() {
-      return odometry;
-    }
+    // public Odometry getOdometry() {
+    //   return null;
+    // }
 
     public void coastDrive() {
-      frontLeftAngleMotor.setIdleMode(IdleMode.kCoast);
-      frontRightAngleMotor.setIdleMode(IdleMode.kCoast);
-      backLeftAngleMotor.setIdleMode(IdleMode.kCoast);
-      backRightAngleMotor.setIdleMode(IdleMode.kCoast);
+      // frontLeftAngleMotor.setIdleMode(IdleMode.kCoast);
+      // frontRightAngleMotor.setIdleMode(IdleMode.kCoast);
+      // backLeftAngleMotor.setIdleMode(IdleMode.kCoast);
+      // backRightAngleMotor.setIdleMode(IdleMode.kCoast);
 
-      frontLeftSpeedMotor.setIdleMode(IdleMode.kCoast);
-      frontRightSpeedMotor.setIdleMode(IdleMode.kCoast);
-      backLeftSpeedMotor.setIdleMode(IdleMode.kCoast);
-      backRightSpeedMotor.setIdleMode(IdleMode.kCoast);
+      // frontLeftSpeedMotor.setIdleMode(IdleMode.kCoast);
+      // frontRightSpeedMotor.setIdleMode(IdleMode.kCoast);
+      // backLeftSpeedMotor.setIdleMode(IdleMode.kCoast);
+      // backRightSpeedMotor.setIdleMode(IdleMode.kCoast);
     }
 }
--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,25 +1,35 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 
+import edu.wpi.first.wpilibj.DutyCycle;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
 
     private CANSparkMax pivotMotor;
-    private Talon telescopeMotor;
-    public ArmSubsystem(CANSparkMax pivotMotor, Talon telescopeMotor) {
+    private TalonFX telescopeMotor;
+    private DutyCycleEncoder pivotEncoder;
+    public ArmSubsystem(CANSparkMax pivotMotor, TalonFX telescopeMotor) {
         this.pivotMotor = pivotMotor;
         this.telescopeMotor = telescopeMotor;
+        this.pivotEncoder = new DutyCycleEncoder(0);
     }
 
     public void setPivotMotor(double speed) {
-        pivotMotor.set(speed);
+        if (speed != 0)
+            System.out.println(pivotEncoder.get());
+        pivotMotor.setVoltage(speed);
     }
 
     public void setTelescopeMotor(double speed){
-        telescopeMotor.set(speed);
+        telescopeMotor.set(ControlMode.Current, speed);
     }
     
 }
