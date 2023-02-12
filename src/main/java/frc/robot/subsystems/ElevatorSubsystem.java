68fcf5c0345ebc35a851600bacfcc6ce9349e4df
BrandonWerbel <brandon@werbel.org>
2023-02-12 17:01:11 -0500

    top limit switch

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -2,9 +2,13 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ElevatorSubsystem extends SubsystemBase {
+
+    DigitalInput topLimitSwitch = new DigitalInput(3);
+    // DigitalInput bottomLimitSwitch = new DigitalInput(0);
 
     private CANSparkMax elevatorMotor;
     public ElevatorSubsystem(CANSparkMax elevatorMotor) {
@@ -12,7 +16,10 @@ public class ElevatorSubsystem extends SubsystemBase {
     }
 
     public void setElevatorMotor(double speed) {
-        elevatorMotor.set(speed);
+        if (topLimitSwitch.get() && speed < 0)
+            elevatorMotor.set(0);
+        else
+            elevatorMotor.set(speed);
     }
     
 }
